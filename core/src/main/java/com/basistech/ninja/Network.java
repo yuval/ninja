445958c0ed5ccb3bc18904a0fa0ccc6848de03fa
Joel Barry <joelb@basistech.com>
2015-05-25 14:33:59 -0400

    Initial work on reading model

--- core/src/main/java/com/basistech/ninja/Network.java
+++ core/src/main/java/com/basistech/ninja/Network.java
@@ -19,7 +19,17 @@
 
 package com.basistech.ninja;
 
+import com.google.common.base.Charsets;
+import com.google.common.collect.Lists;
 import org.ejml.simple.SimpleMatrix;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
 
 public class Network {
     private final SimpleMatrix[] w;
@@ -29,9 +39,19 @@ public class Network {
         this(Functions.SIGMOID, w);
     }
 
+    public Network(List<SimpleMatrix> w) {
+        this(Functions.SIGMOID, w);
+    }
+
     public Network(Function activationFunction, SimpleMatrix ... w) {
         this.activationFunction = activationFunction;
         this.w = w;
+    }
+
+    public Network(Function activationFunction, List<SimpleMatrix> w) {
+        this.activationFunction = activationFunction;
+        this.w = new SimpleMatrix[w.size()];
+        w.toArray(this.w);
     }
 
     private SimpleMatrix addBiasUnit(SimpleMatrix m) {
@@ -43,7 +63,26 @@ public class Network {
         return result;
     }
 
-    SimpleMatrix apply(double ... values) {
+    public int getNumLayers() {
+        return w.length + 1;
+    }
+
+    // 0-based; doesn't count bias unit
+    public int getNumNeurons(int layer) {
+        if (layer == 0) {
+            // subtract the bias unit
+            return w[layer].numCols() - 1;
+        } else {
+            return w[layer - 1].numRows();
+        }
+    }
+
+    // 0-based
+    public SimpleMatrix getWeightMatrix(int layer) {
+        return w[layer].copy();
+    }
+
+    public SimpleMatrix apply(double ... values) {
         int layers = w.length + 1;
         SimpleMatrix[] a = new SimpleMatrix[layers];
         a[0] = addBiasUnit(new SimpleMatrix(values.length, 1, true, values));
@@ -54,5 +93,76 @@ public class Network {
             }
         }
         return a[layers - 1];
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (SimpleMatrix m : w) {
+            sb.append(m.toString());
+        }
+        return sb.toString();
+    }
+
+    public static Network fromText(File file) throws IOException {
+        try (Reader reader = new InputStreamReader(new FileInputStream(file), Charsets.US_ASCII)) {
+            return Network.fromText(reader);
+        }
+    }
+
+    // TODO: better error messages, e.g. line number, invalid header fields, too many lines, etc.
+    public static Network fromText(Reader reader) throws IOException {
+        int numLayers = 0;
+        List<Integer> layerSizes = Lists.newArrayList();
+        BufferedReader br = new BufferedReader(reader);
+        String line;
+        while ((line = br.readLine()) != null) {
+            line = line.trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            if ("w".equals(line)) {
+                break;
+            }
+            int pos = line.indexOf('=');
+            String key = line.substring(0, pos).trim();
+            String value = line.substring(pos + 1).trim();
+            if ("num_layers".equals(key)) {
+                numLayers = Integer.parseInt(value);
+            } else if ("layer_sizes".equals(key)) {
+                for (String field : value.split(" ")) {
+                    layerSizes.add(Integer.parseInt(field));
+                }
+            }
+        }
+
+        List<SimpleMatrix> w = Lists.newArrayList();
+        for (int l = 1; l < numLayers; l++) {
+            int rows = layerSizes.get(l);
+            int cols = layerSizes.get(l - 1) + 1;
+            SimpleMatrix m = new SimpleMatrix(rows, cols);
+            int row = 0;
+            while (row < rows) {
+                line = br.readLine();
+                if (line == null) {
+                    break;
+                }
+                line = line.trim();
+                if (line.isEmpty()) {
+                    continue;
+                }
+                String[] fields = line.split(" ");
+                if (fields.length != cols) {
+                    throw new RuntimeException("wrong number of columns");
+                }
+                for (int j = 0; j < cols; j++) {
+                    m.set(row, j, Double.parseDouble(fields[j]));
+                }
+                row++;
+            }
+            w.add(m);
+        }
+
+        return new Network(w);
     }
 }
