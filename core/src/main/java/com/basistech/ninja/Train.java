db322af0223eba5b176ac375b1d41e658646457a
Yuval <yuval@basistech.com>
2015-07-31 15:27:10 -0400

    First impl of Stochastic GD

--- core/src/main/java/com/basistech/ninja/Train.java
+++ core/src/main/java/com/basistech/ninja/Train.java
@@ -43,7 +43,20 @@ public class Train {
 
     void train(int batchSize, int epochs, double learningRate, File modelFile) throws IOException {
         readExmaples(examplesFile);
-        net.batchGD(x, y, epochs, learningRate);
+        int numBatches = (int) Math.ceil((double) x.numRows() / batchSize);
+        for (int i = 0; i < epochs; i++) {
+            System.out.println("Epoch: " + i);
+            for (int batchIndex = 0; batchIndex < numBatches; batchIndex++) {
+                int startRow = batchIndex * batchSize;
+                int endRow = (batchIndex + 1) * batchSize;
+                if (endRow > x.numRows()) {
+                    endRow = x.numRows();
+                }
+                SimpleMatrix batchX = x.extractMatrix(startRow, endRow, 0, x.numCols());
+                SimpleMatrix batchY = y.extractMatrix(startRow, endRow, 0, y.numCols());
+                net.stochasticGD(batchX, batchY, learningRate);
+            }
+        }
         net.writeModel(modelFile);
     }
 
@@ -89,6 +102,21 @@ public class Train {
         //void train(int batchSize, int epochs, double learningRate, File modelFile) throws IOException {
         List<Integer> layerSizes = Lists.newArrayList(784, 30, 10);
         Train that = new Train(layerSizes, new File(args[1]));
-        that.train(123, 20, 0.8, new File(args[2]));
+//        size        tp        fn        fp         P         R        F1
+//        0                                    980       963        17        38     0.962     0.983     0.972
+//        1                                   1135      1118        17        27     0.976     0.985     0.981
+//        2                                   1032       973        59        36     0.964     0.943     0.953
+//        3                                   1010       972        38        66     0.936     0.962     0.949
+//        4                                    982       940        42        28     0.971     0.957     0.964
+//        5                                    892       845        47        26     0.970     0.947     0.959
+//        6                                    958       933        25        44     0.955     0.974     0.964
+//        7                                   1028       979        49        36     0.965     0.952     0.958
+//        8                                    974       924        50        54     0.945     0.949     0.947
+//        9                                   1009       949        60        49     0.951     0.941     0.946
+//
+//        Total counts:                      10000      9596       404       404         -         -         -
+//                Macro Average:                         -         -         -         -     0.960     0.959     0.959
+//        Micro Average:                         -         -         -         -     0.960     0.960     0.960
+        that.train(100, 15, 0.7, new File(args[2]));
     }
 }
